5e7a975222db5005ca78e3307454a71407d91749
MutantOctopus <bhustus@utc4me.org>
2015-10-10 23:51:38 -0400

    Lots of things
    Kenney game assets downloaded, sketch created, transfer script begun

---
+++ SkillsUSA Competition/Assets/Scripts/GravitySim.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace OctoTools {
+    public class GravitySim : MonoBehaviour {
+        public Vector2 gravity = new Vector2 (0, -9.8F);
+        Rigidbody2D r;
+
+        // Use this for initialization
+        void Start () {
+            r = GetComponent<Rigidbody2D> ();
+        }
+
+        // Update is called once per frame
+        void Update () {
+
+        }
+
+        public void FixedUpdate () {
+            r.AddForce (gravity, ForceMode2D.Force);
+        }
+    }
+}
---
+++ SkillsUSA Competition/Assets/Scripts/TransferRegion.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+#if UNITY_EDITOR
+[ExecuteInEditMode]
+#endif
+public class TransferRegion : MonoBehaviour {
+    [SerializeField]
+    private TransferRegion pair;
+    private Vector3 scale;
+    public bool scaleManuallyAltered {
+        get; private set;
+    }
+
+    // Use this for initialization
+    void Start () {
+        scale = gameObject.transform.localScale;
+        scaleManuallyAltered = false;
+    }
+
+    // Update is called once per frame
+    void Update () {
+#if UNITY_EDITOR
+        if (scale != gameObject.transform.localScale) {
+            scaleManuallyAltered = true;
+            scale = gameObject.transform.localScale;
+        }
+        if (pair && scaleManuallyAltered && !pair.scaleManuallyAltered) {
+            pair.transform.localScale = scale;
+        }
+        scaleManuallyAltered = false;
+#endif
+    }
+}
