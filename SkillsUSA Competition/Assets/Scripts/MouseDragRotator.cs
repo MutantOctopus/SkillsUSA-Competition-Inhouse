f933384f60c99beff59ead4d6155a6c4ee1f5cf5
MutantOctopus <bhustus@utc4me.org>
2015-10-16 11:23:08 -0400

    Final leg
    Get to work

---
+++ SkillsUSA Competition/Assets/Scripts/MouseDragRotator.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseDragRotator : MonoBehaviour {
+    [SerializeField]
+    private float RotationMultiplier = 10;
+
+    private Vector3 startingPosition;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0)) {
+            startingPosition = Input.mousePosition;
+        }
+	    if (Input.GetMouseButton(0)) {
+            Vector3 mouseDelta = startingPosition - Input.mousePosition;
+            Vector3 rotation = new Vector3 (-mouseDelta.y, mouseDelta.x, 0);
+            transform.Rotate (rotation * RotationMultiplier, Space.World);
+            startingPosition = Input.mousePosition;
+        }
+	}
+}
